6a6d17f671e53c11e688718c6dd8fb632b541c2e
Anky Funky <yashshringare@gmail.com>
2025-05-03 23:51:59 +0530

    implement react query in all jobs and Edit job route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -75,7 +75,7 @@ const router = createBrowserRouter([
           {
             index: true,
             element: <AddJob />,
-            action: addJobAction,
+            action: addJobAction(queryClient),
           },
           {
             path: "stats",
@@ -86,7 +86,8 @@ const router = createBrowserRouter([
           {
             path: "all-jobs",
             element: <AllJobs />,
-            loader: allJobsLoader,
+            loader: allJobsLoader(queryClient),
+            errorElement: <ErrorElement />,
           },
           {
             path: "profile",
@@ -101,12 +102,12 @@ const router = createBrowserRouter([
           {
             path: "edit-job/:id",
             element: <EditJob />,
-            loader: editJobLoader,
-            action: editJobAction,
+            loader: editJobLoader(queryClient),
+            action: editJobAction(queryClient),
           },
           {
             path: "delete-job/:id",
-            action: deleteJobAction,
+            action: deleteJobAction(queryClient),
           },
         ],
       },
--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -3,31 +3,63 @@ import { JobsContainer, SearchContainer } from "../components";
 import customFetch from "../utils/customFetch";
 import { useLoaderData } from "react-router-dom";
 import { useContext, createContext } from "react";
+// import { QueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 
 const AllJobsContext = createContext();
 
-export const loader = async ({ request }) => {
-  try {
+const allJobsQuery = (params) => {
+  const { search, jobStatus, jobType, sort, page } = params;
+  return {
+    queryKey: [
+      "jobs",
+      search ?? "",
+      jobStatus ?? "all",
+      jobType ?? "all",
+      sort ?? "newest",
+      page ?? 1,
+    ],
+    queryFn: async () => {
+      const { data } = await customFetch.get("/jobs", {
+        params,
+      });
+      return data;
+    },
+  };
+};
+
+export const loader =
+  (queryClient) =>
+  async ({ request }) => {
     const params = Object.fromEntries([
       ...new URL(request.url).searchParams.entries(),
     ]);
 
-    const { data } = await customFetch.get("/jobs", {
-      params,
-    });
+    await queryClient.ensureQueryData(allJobsQuery(params));
+    return { searchValues: { ...params } };
+    // try {
+    //   const params = Object.fromEntries([
+    //     ...new URL(request.url).searchParams.entries(),
+    //   ]);
 
-    return {
-      data,
-      searchValues: { ...params },
-    };
-  } catch (error) {
-    toast.error(error.response.data.msg);
-    return error;
-  }
-};
+    //   const { data } = await customFetch.get("/jobs", {
+    //     params,
+    //   });
+
+    //   return {
+    //     data,
+    //     searchValues: { ...params },
+    //   };
+    // } catch (error) {
+    //   toast.error(error.response.data.msg);
+    //   return error;
+    // }
+  };
 
 const AllJobs = () => {
-  const { data, searchValues } = useLoaderData();
+  // const { data, searchValues } = useLoaderData();
+  const { searchValues } = useLoaderData();
+  const { data } = useQuery(allJobsQuery(searchValues));
 
   return (
     <AllJobsContext.Provider value={{ data, searchValues }}>
